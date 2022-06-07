75af05b4a30e3b9a62ef460c0f89b778747c2827
ahmed <ahmedmahmud1994@hotmail.com>
2022-06-07 23:33:45 +0100

    updated webchat

--- include/webchat.php
+++ include/webchat.php
@@ -4,7 +4,7 @@
     (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
-    s1.src='https://embed.tawk.to/62952fb6b0d10b6f3e74cfd7/1g4bc4oj9';
+    s1.src='https://embed.tawk.to/629fd072b0d10b6f3e763300/1g504cggo';
     s1.charset='UTF-8';
     s1.setAttribute('crossorigin','*');
     s0.parentNode.insertBefore(s1,s0);
